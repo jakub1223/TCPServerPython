a4d7ca58c18f2c2a61de4abfac74ba280e527f0d
gonczor <wiktor.gonczaronek@gmail.com>
2015-07-02 01:25:30 +0200

    changed python version to 2.7

--- Main.py
+++ Main.py
@@ -1,22 +1,21 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 
 import socket
 
 HOST = '127.0.0.1'      # Symbolic name meaning the local host
-PORT = 12345
+PORT = 12345            # Arbitrary non-privileged port
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 conn, addr = s.accept()
 
-print('Connected by', addr)
+print 'Connected by', addr
 
-while 1:
-        data = conn.recv(1024)
-        if not data:
-                break
+data = conn.recv(1024)
 
-print(data)
+print data
+
+#conn.send(data)
 
 conn.close()
